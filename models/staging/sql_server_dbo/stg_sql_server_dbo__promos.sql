e4aa474d3f994c5a6286dce73c92ecfbca64f007
José Pablo  Márquez Megías <jotape736@gmail.com>
2024-11-19 00:04:12 +0000

    ADD staging, packages UPDATE dbt_proyect

---
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
@@ -0,0 +1,25 @@
+WITH src_promos AS (
+    SELECT * 
+    FROM {{ source('sql_server_dbo', 'promos') }}
+),
+
+renamed_casted AS (
+    SELECT
+        md5('No-promo') AS promo_id,
+        'No-promo' AS promo_desc,
+        0 / 10 AS discount,
+        'inactive' AS status,
+        CONVERT_TIMEZONE('UTC', 'Europe/Paris', CURRENT_TIMESTAMP()) AS _fivetran_synced,
+        NULL AS _fivetran_deleted
+    UNION ALL
+    SELECT
+        md5(promo_id) AS promo_id,
+        INITCAP(promo_id) AS promo_desc,
+        discount,
+        status,
+        CONVERT_TIMEZONE('UTC', 'Europe/Paris',  CAST(_fivetran_synced AS TIMESTAMP_NTZ)) AS _fivetran_synced, -- Pero esto no estaba ya en TIMESTAMP_TZ(9)??
+        _fivetran_deleted
+    FROM src_promos
+)
+
+SELECT * FROM renamed_casted

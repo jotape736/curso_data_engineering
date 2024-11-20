38776e1c15129a36faf6cd33c69c395b9a4efbab
José Pablo  Márquez Megías <jotape736@gmail.com>
2024-11-20 00:07:10 +0000

    UPDATE promos staging

--- models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
@@ -9,18 +9,25 @@ renamed_casted AS (
         'No-promo' AS promo_desc,
         0 / 10 AS discount,
         'inactive' AS status,
-        CONVERT_TIMEZONE('UTC', 'Europe/Paris', CURRENT_TIMESTAMP()) AS _fivetran_synced,
+        --CONVERT_TIMEZONE('UTC', 'Europe/Paris', CURRENT_TIMESTAMP()) AS load_date,
+        null as load_date,
         NULL AS _fivetran_deleted
     UNION ALL
     SELECT
-        md5(promo_id) AS promo_id,
+        {{ dbt_utils.generate_surrogate_key(['promo_id']) }} AS promo_id,
         INITCAP(promo_id) AS promo_desc,
         -- CAST(discount / 10 AS FLOAT) AS discount, por qué con esto en snowflake se ve como un entero?
         discount,
         status,
-        CONVERT_TIMEZONE('UTC', 'Europe/Paris',  CAST(_fivetran_synced AS TIMESTAMP_NTZ)) AS _fivetran_synced, -- Pero esto no estaba ya en TIMESTAMP_TZ(9)??
+        _fivetran_synced AS load_date,
         _fivetran_deleted
     FROM src_promos
 )
 
-SELECT * FROM renamed_casted
+SELECT
+    promo_id,
+    promo_desc,
+    discount,
+    status,
+    load_date
+FROM renamed_casted
